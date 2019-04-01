4d7afe82276f8762cd5d3705f959c78a17c4fa72
Khyati Contractor <44378055+khyatiContractor@users.noreply.github.com>
2019-04-01 15:24:17 -0700

    Removing config for security purpose

--- NewEngnTest/src/environments/environment.ts
+++ NewEngnTest/src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyASqHO3ALDDlpXXUF_-fVGTcvf6qOzje2A",
-    authDomain: "newengentest.firebaseapp.com",
-    databaseURL: "https://newengentest.firebaseio.com",
-    projectId: "newengentest",
-    storageBucket: "newengentest.appspot.com",
-    messagingSenderId: "262796706344"
+  ffirebaseConfig : {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
 
